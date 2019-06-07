66b7d0aa3a5dfe4937154d4aa6be7e9b29b5593c
Sergey James <cd57@mail.ru>
2019-06-07 11:27:21 +0300

    add noexcept specifier

--- algorithms.h
+++ algorithms.h
@@ -1,5 +1,5 @@
 /**
-* @brief wrappers over standard algorithms, to minimize code size, and make more readable 
+* @brief wrappers over standard algorithms, to minimize code size, and make more readable
 *
 * @author Sergey Masuryan
 * Contact Telegram: @SergeyJamesM
@@ -17,16 +17,30 @@
 #include <vector>
 
 /**
-*	Required     : 
+*	Required     :
 *	Complexity   : At most last - first applications of the predicate
 *	Return value :
-*	false if the range [first, last) is empty, or no such element is found in range [first, last).
-*   true  if the range [first, last) contains const Ty& Val.
+*		false if the range [first, last) is empty, or no such element is found in range [first, last).
+*		true  if the range [first, last) contains const Ty& Val.
 */
 template<class Init, class Ty>
-inline bool contains(Init begin, Init end, const Ty& Val)
+inline bool contains(Init begin, Init end, const Ty& Val) noexcept
 {
 	return (std::find(begin, end, Val) != end);
+}
+
+
+/**
+*	Required     :
+*	Complexity   : At most last - first applications of the predicate
+*	Return value :
+*		false if the range [first, last) is empty, or no such element predicate pred returns true in range [first, last).
+*   	true  if the range [first, last) an element for which predicate pred returns true
+*/
+template<class Init, class Pr>
+inline bool contains_if(Init begin, Init end, Pr pred) noexcept
+{
+	return (std::find_if(begin, end, pred) != end);
 }
 
 
@@ -36,7 +50,7 @@ inline bool contains(Init begin, Init end, const Ty& Val)
 *	Return value : (none)
 */
 template<class T>
-inline void quik_remove_at(std::vector<T>& v, std::size_t idx)
+inline void quik_remove_at(std::vector<T>& v, std::size_t idx) noexcept
 {
 	if (idx < v.size()) {
 		v[idx] = std::move(v.back());
@@ -48,12 +62,12 @@ inline void quik_remove_at(std::vector<T>& v, std::size_t idx)
 /**
 *	Required     : sorted container (class C)
 *	Complexity   :
-			1-2) Logarithmic in the size of the container, O(log(size())).
-			3-4) Amortized constant if the insertion happens in the position just after the hint, logarithmic in the size of the container otherwise.(until C++11)
-			3-4) Amortized constant if the insertion happens in the position just before the hint, logarithmic in the size of the container otherwise. (since C++11)
-			5-6) O(N*log(size() + N)), where N is the number of elements to insert.
-			7)   Logarithmic in the size of the container, O(log(size())).
-			8)   Amortized constant if the insertion happens in the position just before the hint, logarithmic in the size of the container otherwise.
+1-2) Logarithmic in the size of the container, O(log(size())).
+3-4) Amortized constant if the insertion happens in the position just after the hint, logarithmic in the size of the container otherwise.(until C++11)
+3-4) Amortized constant if the insertion happens in the position just before the hint, logarithmic in the size of the container otherwise. (since C++11)
+5-6) O(N*log(size() + N)), where N is the number of elements to insert.
+7)   Logarithmic in the size of the container, O(log(size())).
+8)   Amortized constant if the insertion happens in the position just before the hint, logarithmic in the size of the container otherwise.
 *	Return value : (none)
 */
 template<class C, class T>
@@ -66,12 +80,23 @@ inline void insert_sorted(C & c, const T& item)
 *	Since        :	C++14 (and higer).
 *	Complexity   :	For nonempty ranges, exactly std::distance(first,last) -1 applications of the corresponding predicate.
 *	Return value :	Forward iterator to the new end of the range
-*	USage        :	Erase–remove idiom.
+*	USage        :	Erase-remove idiom.
 */
 template<class FwdIt>
-inline FwdIt remove_multi_wshitespace(FwdIt begin, FwdIt end)
+inline FwdIt remove_multi_wshitespace(FwdIt begin, FwdIt end) noexcept
 {
 	return std::unique(begin, end, [](const auto& a, const auto& b) {  return isspace(a) && isspace(b); });
+}
+
+/*
+*	Complexity   :	Logarithmic
+*	Return value :	nums count, min val is 1
+*/
+inline int calc_nums_count(int a) noexcept
+{
+	int n{ 1 };
+	while ((a /= 10) > 0) ++n;
+	return n; // NRVO optimization
 }
 
 #endif // !experiments_algorithms_hpp
