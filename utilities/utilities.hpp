96d7f0ac19af56431d46eb80a5ddb78859710d38
Sergey Masuryan <s.masuryan@visionlabs.ru>
2019-09-05 15:23:48 +0300

    add utilities.hpp

---
+++ utilities/utilities.hpp
@@ -0,0 +1,39 @@
+/**
+* @brief some utilities collection from famous c++ experts
+* @brief cross-platform one-file C++ header
+*
+* @author Sergey Masuryan
+* Contact Telegram: @SergeyJamesM
+*
+*/
+#pragma once
+#ifndef utils_utils_hpp
+#define utils_utils_hpp
+
+namespace wrp {
+
+	/** @copyright ©  : @herbsutter
+	 *	@brief        : сheck if First (type _Derived) is Derived Of Second (type _Base)
+	 *	@required     :
+	 *	@usage        : 
+	 *          class parent {};
+	 *          class child : public parent {};
+	 *
+	 *          isFirstDerivedOfSecond<parent, child__>::getResult(); //< false
+	 *          isFirstDerivedOfSecond<child, parent>::getResult();   //< true
+	 *          
+    **/
+    template<typename _Derived, typename _Base>
+    class isFirstDerivedOfSecond {
+		class No { };
+		class Yes { No no[2]{}; };
+		static constexpr Yes Test( _Base* );
+		static constexpr No Test( ... );
+	public:
+		static constexpr bool getResult() noexcept { 
+			return { sizeof(Test(static_cast<_Derived*>(0))) == sizeof(Yes) };
+		}
+	};
+} // ! namespace wrp
+
+#endif // ! utils_utils_hpp
