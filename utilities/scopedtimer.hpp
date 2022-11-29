3daea5fb7342e5e622b340fd1c53da452646e889
Jmaes <cd57@mail.ru>
2022-11-29 13:30:08 +0300

    Turned to `high_resolution_clock`.

--- utilities/scopedtimer.hpp
+++ utilities/scopedtimer.hpp
@@ -50,7 +50,7 @@ namespace wrp {
 	class ScopedTimer {
 	public:
 		explicit ScopedTimer(bool printOnExit = false) noexcept
-			: m_start{ std::chrono::system_clock::now() },
+			: m_start{ std::chrono::high_resolution_clock::now() },
 			  m_printOnExit{ printOnExit }
 		{}
 		
@@ -58,17 +58,17 @@ namespace wrp {
 			if (m_printOnExit) {
 				std::cout <<
 				"Elapsed: " <<
-				std::chrono::duration_cast<_duration>(std::chrono::system_clock::now() - m_start).count() <<
+				std::chrono::duration_cast<_duration>(std::chrono::high_resolution_clock::now() - m_start).count() <<
 				" " << chronoDurationPrettyName<_duration>() << std::endl;
 			}
 		}
 
 		size_t getCurDuration() const noexcept {
-			return std::chrono::duration_cast<_duration>(std::chrono::system_clock::now() - m_start).count();
+			return std::chrono::duration_cast<_duration>(std::chrono::high_resolution_clock::now() - m_start).count();
 		}
 	
 	private:
-		std::chrono::time_point<std::chrono::system_clock> m_start;
+		std::chrono::time_point<std::chrono::high_resolution_clock> m_start;
 		const bool m_printOnExit;
 
 		ScopedTimer(ScopedTimer&&) = delete;
